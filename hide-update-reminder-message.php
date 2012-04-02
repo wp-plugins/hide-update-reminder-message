aa2053c2e861abe18af24c44161da8e37e0742d2
sumitengg01 <sumitengg01@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-02 17:58:52 +0000

    add my plugin
    git-svn-id: https://plugins.svn.wordpress.org/hide-update-reminder-message/trunk@526353 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hide-update-reminder-message.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Plugin Name: Hide Update Message From Admin
+Plugin URI: http://www.sumitbansal.com/
+Description: Allows you to remove the upgrade Nag screen from the wordpress adminstrator or other users
+Author: Sumit Bansal
+Version: 1.0
+Author URI: http://www.sumitbansal.com/
+*/
+if ( is_admin() ){
+	add_action('admin_menu', 'add_custom_link');
+	function add_custom_link() {
+		add_options_page('Hide Update Reminder', 'Hide Update Reminder', 'administrator', 'update-reminder', 'hide_reminder_setting');
+	}
+	
+	function hide_reminder_setting(){
+		if($_POST['option_hide'] == 'Y') {  
+			$users = $_POST['sumit_users'];  
+			update_option('sumit_users', $users);
+			?>
+			<div class="updated"><p><strong><?php _e('Options saved.' ); ?></strong></p></div>
+			<?php 
+		}  
+			?>
+			<div class="wrap">  
+				<?php    echo "<h2>" . __( 'Hide Update Reminder', 'sumit_bansal' ) . "</h2>"; ?>  
+          
+                <form name="oscimp_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">  
+                    <input type="hidden" name="option_hide" value="Y">  
+                    <?php    echo "<h4>" . __( 'Hide Reminder Setting', 'sumit_bansal' ) . "</h4>"; ?>  
+                    <p><?php _e("Select Option: " ); ?><select name="sumit_users"><option value="users"<?php if(get_option('sumit_users')=='users') echo 'selected="selected"'; ?>>Only Users</option><option value="ausers" <?php if(get_option('sumit_users')=='ausers') echo 'selected="selected"'; ?>>Users + Admin</option></select></p>  
+                    <p class="submit">  
+                    <input type="submit" name="Submit" value="<?php _e('Update Options', 'sumit_bansal' ) ?>" />  
+                    </p>  
+                </form>  
+			</div>
+			<?php
+	}
+}
+add_action('admin_init', 'hide_message');
+
+function hide_message($matches) {
+	global $userdata;
+	if(get_option('sumit_users')=='users'):
+		if (!current_user_can('update_plugins')):
+			remove_action('admin_notices', 'update_nag', 3);
+		endif;
+	elseif(get_option('sumit_users')=='ausers'):
+			remove_action('admin_notices', 'update_nag', 3);
+	else:
+		if (!current_user_can('update_plugins')):
+			remove_action('admin_notices', 'update_nag', 3);
+		endif;
+	endif;
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== Hide Update Reminder ===
+Contributors: sumit
+Donate link: http://www.sumitbansal.com
+Tags: admin, upgrade, cms
+Requires at least: 2.5
+Tested up to: 3.0.1
+Stable tag: 1.0
+
+Hides the Update Reminder in the Admin for all non Admin users.
+
+== Description ==
+
+With the use of this plugin you can hides update reminder message from the WordPress Admin for all non admin users and for Admin also.
+
+You can change the setting from the WordPress Admin to show the update reminder message will hide only for only non admin users or Admin to.
+
+== Installation ==
+
+Installation Instructions:
+
+    Download the plugin and unzip it.
+    Put the 'hide-update-reminder-message' folder into your wp-content/plugins/ directory.
+    Go to the Plugins page in your WordPress Admin and activate the Hide Update Reminder Message
+    Go to setting->Hide Update Reminder Tab make the appropriate setting according to you.
+
+== Frequently Asked Questions ==
+
+
+== Screenshots ==
+
+
+== Support ==
+
+Support is provided at http://sumitbansal.com/wordpress-plugin/hide-update-reminder-message/
